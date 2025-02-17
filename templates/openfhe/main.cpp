abe3ada3dce4c50f2c76325987f3deea0b5bd5eb
novamind <annodonaris@gmail.com>
2025-02-17 09:52:33 +0100

    Add templates

---
+++ templates/openfhe/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "yourSolution"
+
+using namespace lbcrypto;
+
+int main(int argc, char *argv[])
+{
+    std::string pubKeyLocation;
+    std::string multKeyLocation;
+    std::string rotKeyLocation;
+    std::string ccLocation;
+    std::string inputLocation;
+    std::string outputLocation;
+
+    for (auto i = 1; i < argc; i += 2)
+    {
+        std::string arg = argv[i];
+        if (arg == "--key_pub")
+        {
+            pubKeyLocation = argv[i + 1];
+        }
+        else if (arg == "--key_mult")
+        {
+            multKeyLocation = argv[i + 1];
+        }
+        else if (arg == "--key_rot")
+        {
+            rotKeyLocation = argv[i + 1];
+        }
+        else if (arg == "--cc")
+        {
+            ccLocation = argv[i + 1];
+        }
+        else if (arg == "--input")
+        {
+            inputLocation = argv[i + 1];
+        }
+        else if (arg == "--output")
+        {
+            outputLocation = argv[i + 1];
+        }
+    }
+
+    yourSolution yourSolution(ccLocation, pubKeyLocation, multKeyLocation, rotKeyLocation, inputLocation, 
+                                  outputLocation);
+    yourSolution.eval();
+    yourSolution.serializeOutput();
+    return 0;
+}   
